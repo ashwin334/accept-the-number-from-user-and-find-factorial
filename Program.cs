7b11498a94195574132ac349b2088171e0b4a034
ashwin333 <ashwinpatil832@gmail.com>
2022-10-27 22:02:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace accept_the_number_from_user_and_find_factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, fact = 1;
+            Console.WriteLine("enter the number");
+            num = Convert.ToInt32(Console.ReadLine());  
+            for(int i=1; i<=num; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine(fact);
+        }
+    }
+}
---
+++ armstrong number.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace accept_the_number_from_user_and_find_factorial
+{
+    internal class armstrong_number
+    {
+        static void Main(string[] args)
+        {
+           int num, sum = 0, temp, rem;
+           Console.WriteLine("Enter the number");
+           num=Convert.ToInt32(Console.ReadLine());
+           temp = num;
+           while(num >0)
+            {
+             rem =num% 10;
+            sum=sum +rem+rem* rem;
+            num=num/10;
+
+            }
+            if (temp == sum)
+           {
+           Console.WriteLine("its a armstrong number");
+           }else
+            Console.WriteLine("not a armstrong number");
+           Console.ReadLine();
+
+           }
+    }
+}
